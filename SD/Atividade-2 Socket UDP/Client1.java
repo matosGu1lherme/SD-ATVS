c61cd6c48fd4ef9647b33b7e009ea848c8765609
GuilhermeMatos <87232672+matosGu1lherme@users.noreply.github.com>
2023-09-18 15:06:06 -0300

    Add files via upload

--- SD/Atividade-2 Socket UDP/Client1.java
+++ SD/Atividade-2 Socket UDP/Client1.java
@@ -1,40 +1,97 @@
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.*;
 import java.nio.ByteBuffer;
+import java.util.Scanner;
+import java.io.*;
 
-
-public class Client1 { 
-    
+public class Client1 {
+ 
     public static void main(String[] args) {
-        String nick = "Guilherme";
-        String mensagem = "oi bom dia";
-
-        byte[] cabecalho = create_mensage(nick, mensagem);
-        
-        DatagramSocket dgramSocket;
-
+        System.out.println("Iniciando Cliente-1");
+        DatagramSocket dgramSocket = null;
         try {
-            dgramSocket = new DatagramSocket(6661);
+            dgramSocket = new DatagramSocket(6666);
 
-            InetAddress serverAddr = InetAddress.getByName("127.0.0.1");
-            int serverPort = 6666;
-
-            DatagramPacket request = new DatagramPacket(cabecalho, cabecalho.length, serverAddr, serverPort);
-
-            dgramSocket.send(request);
+            ReciveThread r = new ReciveThread(dgramSocket);
+            SendThread s = new SendThread(dgramSocket, 6661);
+            r.start();
+            s.start();
 
         }catch (SocketException e) {
             System.out.println("Socket: " + e.getMessage());
-        }catch (IOException e) {
-            System.out.println("IO: " + e.getMessage());
         }
-
     }
+}
+
+//-------------------Thread de Recebimento-------------------------//
+
+class ReciveThread extends Thread {
+
+    DatagramSocket dgramSocket;
+
+    public ReciveThread(DatagramSocket dgramSocket) {
+        this.dgramSocket = dgramSocket;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Recive Socket INICIADO");
+        while (true){
+            try {
+                byte[] buffer = new byte[1000];
+                DatagramPacket dgramPacket = new DatagramPacket(buffer, buffer.length);
+                dgramSocket.receive(dgramPacket);
+                
+                byte[] mensageType = new byte[4];
+                System.arraycopy(dgramPacket.getData(), 0, mensageType, 0, 4);
+                int mtype = ByteBuffer.wrap(mensageType).getInt();
+                System.out.println(mtype);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
+//-------------------Thread de Envio-------------------------//
+
+class SendThread extends Thread {
+
+    DatagramSocket dgramSocket;
+    InetAddress serverAddr;
+    int serverPort;
     
+    public SendThread(DatagramSocket dgramSocket, int serverPort) {
+        try {
+            this.dgramSocket = dgramSocket;
+            serverAddr = InetAddress.getByName("127.0.0.1");
+            serverPort = serverPort;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        System.out.println("SEND Socket INICIADO");
+        Scanner reader = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println("Cliente-1:");
     
+                String buffer = reader.nextLine();
+    
+                byte[] mensagem = create_mensage("Sergio", buffer);
+    
+                DatagramPacket request = new DatagramPacket(mensagem, mensagem.length, serverAddr, serverPort);
+                dgramSocket.send(request);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static byte[] create_mensage(String nome, String msg) {
 
         byte[] cacalho = new byte[331];
@@ -63,5 +120,4 @@ public class Client1 {
         
         return cacalho;
     }
-
 }
